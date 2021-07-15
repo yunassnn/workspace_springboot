475aa2697142af59315c6af9ed0ec76dcf83a476
yunassnn <kasspo20@gmail.com>
2021-07-15 23:38:41 +0900

    Member dto annotation 수정

--- step02/src/main/java/com/work/dto/Member.java
+++ step02/src/main/java/com/work/dto/Member.java
@@ -2,14 +2,13 @@ package com.work.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@ToString(exclude = {"memberPw", "mobile"}, includeFieldNames = false)
+@Data
 @NoArgsConstructor
 @Builder
 @AllArgsConstructor
